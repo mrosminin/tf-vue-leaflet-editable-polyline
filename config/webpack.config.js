164336d811d2f22caee317889210a6556523498c
mrosminin <leonid@osminin.com>
2021-10-10 15:20:49 +0300

    package upgrade

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
                     'sass-loader',
                 ]
             },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
         ]
     },
     plugins: [
